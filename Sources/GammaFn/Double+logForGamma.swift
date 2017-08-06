f64405cb591120498089bd3694883fd5a761f185
Matheus Brasil <matheus.brasil10@gmail.com>
2017-08-06 19:16:49 -0300

    feat(core):  handle the logarithm of the gamma function as a special function in its own right

---
+++ Sources/GammaFn/Double+logForGamma.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+extension Double {
+    
+    public static func logForGamma(of value: Double) -> Double {
+        
+        do {
+            try value.isNegative()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        var x = Array.pLn[0];
+        
+        for i in (0..<Array.pLn.count - 1).reversed() {
+            let index = Double(i)
+            x += Array.pLn[i] / (value + index);
+        }
+        
+        let t = value + Double.gLn + 0.5;
+        
+        return
+            0.5
+                * log(2 * Double.pi)
+                + (value + 0.5)
+                * log(t)
+                - t
+                + log(x)
+                - log(value)
+    }
+    
+    public var logValueForGamma: Double {
+        get {
+            return Double.logForGamma(of: self)
+        }
+    }
+    
+}
